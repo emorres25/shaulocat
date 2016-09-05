787dd50029449b076959e611d5a0068c556b8624
Shaurya <Shaurya@Shauryas-MacBook-Pro.local>
2016-09-05 15:45:18 +0530

    proc

--- app/views.py
+++ app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+import requests
+
 
 # Create your views here.
+YO_API = "https://api.justyo.co/yo/"
+api_key = '006c7d21-b262-40e9-b243-d4082436f646'
+
+def send_yo(username, link):
+    """Yo a username"""
+    requests.post(
+        YO_API,
+        data={'api_token': api_token, 'username': username, 'link': link})
+
+def yo():
+    """Handle callback request"""
+    username = request.args.get('username')
+    location = request.args.get('location')
+    splitted = location.split(';')
+    # Parse latitude and longitude from request params
+    latitude = splitted[0]
+    longitude = splitted[1]
+    requests.post("http://api.justyo.co/yo/", 
+              data={'api_token': api_token, 'username': 'EMORRES25'})
+    return 'OK'
