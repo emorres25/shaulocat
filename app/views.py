7f33e12c69b266e50e00440d8af17cfec4dd0ac6
Shaurya <Shaurya@Shauryas-MacBook-Pro.local>
2016-09-05 16:12:40 +0530

    proc

--- app/views.py
+++ app/views.py
@@ -8,9 +8,7 @@ api_key = '006c7d21-b262-40e9-b243-d4082436f646'
 
 def send_yo(username, link):
     """Yo a username"""
-    requests.post(
-        YO_API,
-        data={'api_token': api_token, 'username': username, 'link': link})
+    requests.post(YO_API, data={'api_token': api_key, 'username': username, 'link': link})
 
 def yo():
     """Handle callback request"""
